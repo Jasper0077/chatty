cdd5ec558931bb900395ef7f4a5110fdce9a3825
Jasper0077 <jasperhwong@hotmail.com>
2023-05-13 16:30:52 +0800

    minor update on auth page

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -11,13 +11,15 @@ import {
     UseFormRegisterReturn,
     useForm
 } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 import Input from "@/app/components/Input";
 import Button from "@/app/components/Button";
-
-type Variant = "login" | "register";
+import AuthSocialButton from "./AuthSocialButton";
 
 const AuthForm = () => {
-    const [variant, setVariant] = React.useState<Variant>("register");
+    const [variant, setVariant] = React.useState<"login" | "register">(
+        "register"
+    );
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
     const toggleVariant = React.useCallback(() => {
@@ -26,7 +28,7 @@ const AuthForm = () => {
         } else {
             setVariant("login");
         }
-    }, []);
+    }, [variant]);
 
     const {
         register,
@@ -60,11 +62,8 @@ const AuthForm = () => {
     return (
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
             <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-                {variant === "register" && (
-                    <form
-                        className="space-y-6"
-                        onSubmit={handleSubmit(onSubmit)}
-                    >
+                <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                    {variant === "register" && (
                         <Input
                             label="Name"
                             id="name"
@@ -72,27 +71,64 @@ const AuthForm = () => {
                             register={register}
                             errors={errors}
                         />
-                        <Input
-                            label="Email Address"
-                            id="email"
-                            type="email"
-                            register={register}
-                            errors={errors}
-                        />
-                        <Input
-                            label="Password"
-                            id="password"
-                            type="password"
-                            register={register}
-                            errors={errors}
-                        />
-                        <div>
-                            <Button type="submit">
-                                {variant === "register" ? "Sign In" : "Login"}
-                            </Button>
+                    )}
+                    <Input
+                        label="Email Address"
+                        id="email"
+                        type="email"
+                        register={register}
+                        errors={errors}
+                    />
+                    <Input
+                        label="Password"
+                        id="password"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                    />
+                    <div className="">
+                        <Button type="submit" center={true} fullWidth>
+                            {variant === "register" ? "Sign In" : "Login"}
+                        </Button>
+                    </div>
+                </form>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="w-full border-t border-gray-600" />
+                        <div className="relative flex justify-center text-sm top-[-12px]">
+                            <span className="bg-white px-2 text-gray-500">
+                                Or continue with
+                            </span>
                         </div>
-                    </form>
-                )}
+                    </div>
+                    <div className="mt-6 flex flex-row gap-2 justify-center items-center">
+                        <AuthSocialButton
+                            Icon={BsGithub}
+                            handler="Github"
+                            onClick={() => socialAction("github")}
+                        />
+                        <AuthSocialButton
+                            Icon={BsGoogle}
+                            handler="Google"
+                            onClick={() => socialAction("google")}
+                        />
+                    </div>
+                    <div className="mt-6 flex flex-row gap-2 justify-center items-center text-sm text-gray-500">
+                        <div>
+                            {variant === "register"
+                                ? "Already have an account?"
+                                : "New to Chatty?"}
+                        </div>
+                        <div
+                            onClick={toggleVariant}
+                            className="underline cursor-pointer"
+                        >
+                            {variant === "register"
+                                ? "Login"
+                                : "Create an account"}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -8,6 +8,7 @@ export interface ButtonProps {
     secondary?: boolean;
     danger?: boolean;
     disabled?: boolean;
+    center?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -17,7 +18,8 @@ const Button: React.FC<ButtonProps> = ({
     onClick,
     secondary,
     danger,
-    disabled
+    disabled,
+    center
 }) => {
     return (
         <button
@@ -27,6 +29,7 @@ const Button: React.FC<ButtonProps> = ({
             className={cn(
                 `flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
                 disabled && "opacity-50 cursor-default",
+                center && "mx-auto",
                 fullWidth && "w-full",
                 secondary ? "text-gray-900" : "text-white",
                 danger &&
