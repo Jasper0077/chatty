fdee9c7eb05c7e55e94b151f492d9cecae2f4021
Jasper0077 <jasperhwong@hotmail.com>
2023-05-17 22:37:37 +0800

    update models

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,6 +1,16 @@
+import { IconType } from "react-icons";
+
 export interface ServiceResponse {
     ok: boolean;
     message: string;
     data: any;
     errors: string | null;
+}
+
+export interface Route {
+    label: string;
+    href: string;
+    Icon: IconType;
+    onClick?: () => void;
+    active: boolean;
 }
