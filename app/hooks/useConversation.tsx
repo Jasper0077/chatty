01bc7f25ff3c6218b8157465002a5ea99fda40df
Jasper0077 <jasperhwong@hotmail.com>
2023-05-17 22:37:25 +0800

    create useConversation hook

---
+++ app/hooks/useConversation.tsx
@@ -0,0 +1,17 @@
+import { useParams } from "next/navigation";
+import React from "react";
+
+const useConversation = () => {
+    const params = useParams();
+    const conversationId = React.useMemo<string>(() => {
+        return params?.conversationId ? params?.conversationId : "";
+    }, [params?.conversationId]);
+    const isOpen = React.useMemo<boolean>(
+        () => !!conversationId,
+        [conversationId]
+    );
+
+    return { isOpen, conversationId };
+};
+
+export default useConversation;
