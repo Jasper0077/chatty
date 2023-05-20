15d1e85c0493c378f4f333f880e73b75e65cfbe6
Jasper0077 <jasperhwong@hotmail.com>
2023-05-20 22:53:37 +0800

    use getUsers in users layout

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Sidebar from "@/app/components/sidebar/Sidebar";
+import getUsers from "@/app/actions/getUsers";
+import UserList from "./components/UserList";
 
 interface Props {
     children: React.ReactNode;
 }
 
 export default async function UsersLayout({ children }: Props) {
+    const users = await getUsers();
     return (
         // @ts-expect-error Server Component
         <Sidebar>
-            <div className="h-full">{children}</div>
+            <div className="h-full">
+                <UserList users={users} />
+                {children}
+            </div>
         </Sidebar>
     );
 }
