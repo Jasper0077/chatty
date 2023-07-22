cf1295a5e397192237bf3708f8ee989474517924
Jasper0077 <jasperhwong@hotmail.com>
2023-07-22 23:42:41 +0800

    update left right props to button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,6 +9,8 @@ export interface ButtonProps {
     danger?: boolean;
     disabled?: boolean;
     center?: boolean;
+    left?: boolean;
+    right?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -19,7 +21,9 @@ const Button: React.FC<ButtonProps> = ({
     secondary,
     danger,
     disabled,
-    center
+    center,
+    left,
+    right
 }) => {
     return (
         <button
@@ -30,6 +34,8 @@ const Button: React.FC<ButtonProps> = ({
                 `flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
                 disabled && "opacity-50 cursor-default",
                 center && "mx-auto",
+                left && "mr-auto",
+                right && "ml-auto",
                 fullWidth && "w-full",
                 secondary ? "text-gray-900" : "text-white",
                 danger &&
