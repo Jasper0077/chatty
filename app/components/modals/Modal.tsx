abb479a8b8489fa4ebebfcd824bc529f4988d59f
Jasper0077 <jasperhwong@hotmail.com>
2023-07-22 23:43:13 +0800

    fix base modal to use w-max

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -31,7 +31,7 @@ const Modal: React.FC<Props> = ({ isOpen, onClose, children }) => {
                         leave="ease-in duration-200"
                         leaveFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xsl transition-all w-full sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 text-left shadow-xsl transition-all w-full sm:my-8 sm:w-full sm:max-w-max sm:p-6">
                             <div className="absolute right-0 top-0 hidden pr-4 pt-4 sm:block z-10">
                                 <button
                                     type="button"
